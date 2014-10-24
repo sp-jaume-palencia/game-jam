76c347e6348c6e1362d414c0274c8620be742441
Arnau Ginabreda <arnau.ginabreda@socialpoint.es>
2014-10-24 17:14:12 +0200

    Splash fixed

--- test/core/src/com/test/systems/ScreenSystem.java
+++ test/core/src/com/test/systems/ScreenSystem.java
@@ -1,17 +1,32 @@
 package com.test.systems;
 
+import com.badlogic.gdx.scenes.scene2d.Action;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.test.screens.Splashscreen;
 
 public class ScreenSystem {
 
 	public Splashscreen splash;
+	
+	public Action fadeIn;
+	
+	float fadeOutTime = 0.25f;
+    float fadeInTime = 0.25f;
 
 	public void load()
 	{
 		splash = new Splashscreen();
 		
+		fadeIn = Actions.sequence(Actions.fadeOut(0.001f), Actions.fadeIn(fadeInTime), Actions.run(RootSystem.screens.onFadeIn));
 	}
 	
-	
+	public Runnable onFadeIn = new Runnable()
+    {
+        @Override
+        public void run()
+        {
+            fadeIn = Actions.sequence(Actions.fadeOut(0.001f), Actions.fadeIn(fadeInTime), Actions.run(RootSystem.screens.onFadeIn));
+        }
+    };
 
 }
