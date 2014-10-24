54af752c84c4abc0f14e721f8430f5f080cdc7f9
Arnau Ginabreda <arnau.ginabreda@socialpoint.es>
2014-10-24 16:35:16 +0200

    Splash assets

--- test/core/src/com/test/screens/Splashscreen.java
+++ test/core/src/com/test/screens/Splashscreen.java
@@ -1,7 +1,9 @@
 package com.test.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -24,8 +26,8 @@ public class Splashscreen implements Screen
     
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
-		
+		Gdx.gl.glClearColor(0f, 1f, 0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
 
 	@Override
--- test/core/src/com/test/systems/AssetsSystem.java
+++ test/core/src/com/test/systems/AssetsSystem.java
@@ -6,10 +6,12 @@ import com.badlogic.gdx.graphics.Texture;
 public class AssetsSystem
 {
 	public Texture caca;
+	public Texture backSpace;
 	
 	public void load()
 	{
-		//caca = new Texture(Gdx.files.internal("assets/caca.png"));
+		caca = new Texture(Gdx.files.internal("assets/jjinyar.png"));
+		backSpace = new Texture(Gdx.files.internal("assets/bg-mockup.png"));
 		
 	}
 
