6d3a95e3fd2a6118e1d6e3fb838c321ef92c88cb
Arnau Ginabreda <arnau.ginabreda@socialpoint.es>
2014-10-24 20:08:39 +0200

    Splash and fades a little better

--- test/core/src/com/test/systems/ScreenSystem.java
+++ test/core/src/com/test/systems/ScreenSystem.java
@@ -2,7 +2,6 @@ package com.test.systems;
 
 import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.test.game.TestGame;
 import com.test.screens.Splashscreen;
 //import com.test.screens.Menuscreen;
 
@@ -14,8 +13,8 @@ public class ScreenSystem {
 	public Action showGame;
 	public Action fadeIn;
 	
-	float fadeOutTime = 0.25f;
-    float fadeInTime = 0.25f;
+	float fadeOutTime = 0.5f;
+    float fadeInTime = 0.5f;
 
 	public void load()
 	{
@@ -23,7 +22,7 @@ public class ScreenSystem {
 		//menu = new Menuscreen();
 		
 		showGame = Actions.sequence(Actions.fadeOut(fadeOutTime), Actions.run(RootSystem.screens.onActionGame));
-		fadeIn = Actions.sequence(Actions.fadeOut(0.001f), Actions.fadeIn(fadeInTime), Actions.run(RootSystem.screens.onFadeIn));
+		fadeIn = Actions.sequence(Actions.fadeOut(0.0f), Actions.fadeIn(fadeInTime), Actions.run(RootSystem.screens.onFadeIn));
 	}
 	
 	public Runnable onFadeIn = new Runnable()
@@ -31,7 +30,7 @@ public class ScreenSystem {
         @Override
         public void run()
         {
-            fadeIn = Actions.sequence(Actions.fadeOut(0.001f), Actions.fadeIn(fadeInTime), Actions.run(RootSystem.screens.onFadeIn));
+            fadeIn = Actions.sequence(Actions.fadeOut(0.0f), Actions.fadeIn(fadeInTime), Actions.run(RootSystem.screens.onFadeIn));
         }
     };
     
