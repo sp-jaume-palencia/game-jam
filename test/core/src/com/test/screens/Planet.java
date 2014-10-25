aa98687acea4d7f6b47f574c5de75df3008c257a
Arnau Ginabreda <arnau.ginabreda@socialpoint.es>
2014-10-25 23:08:37 +0200

    Oops fix

--- test/core/src/com/test/screens/Planet.java
+++ test/core/src/com/test/screens/Planet.java
@@ -1,5 +1,6 @@
 package com.test.screens;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Group;
