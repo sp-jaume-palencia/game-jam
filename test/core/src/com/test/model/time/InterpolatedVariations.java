d3644636437d8ede755769090118d2aaf5435a2f
Iñaki Frison <inaki.frison@socialpoint.es>
2014-10-25 10:40:12 +0200

    commit

--- test/core/src/com/test/model/time/InterpolatedVariations.java
+++ test/core/src/com/test/model/time/InterpolatedVariations.java
@@ -7,6 +7,8 @@ public class InterpolatedVariations extends TimeVariations {
     public int getValue(int time)  {
         Map.Entry<Integer,Integer> before = timeMap.ceilingEntry(time);
         Map.Entry<Integer,Integer> after = timeMap.floorEntry(time);
+        if (before == null) return after.getValue();
+        if (after == null) return before.getValue();
         int variationPerTimeUnit = (after.getValue() - before.getValue()) / (after.getKey() - before.getKey());
         int interpolation = before.getValue() + (time-before.getKey())*variationPerTimeUnit;
         return interpolation;
